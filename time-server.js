7a363aeb832269645adba35e1670fb1dac165c29
Xavi Leal <lealxavi@gmail.com>
2015-03-18 23:31:04 +0100

    Finished exercise for time-server

--- time-server.js
+++ time-server.js
@@ -1,20 +1,34 @@
 var net = require('net');
 
 var server = net.createServer(function(socket) {
-  console.log("Petición");
+  socket.end(getFormatedDate());
 });
 
-server.listen(8000);
+server.listen(process.argv[2]);
 
 var getFormatedDate = function () {
 
   // 2013-07-06 17:42
 
-  var now = new Date();
+  var now    = new Date();
 
-  var year  = now.getFullYear();
-  var month = ((now.getMonth() + 1) < 10)?"0"+now.getMonth():""+now.getMonth(); 
+  var year   = now.getFullYear();
+  var month  = addLeftCero(now.getMonth() + 1); 
+  var day    = addLeftCero(now.getDate());
+  var hour   = addLeftCero(now.getHours());
+  var minute = addLeftCero(now.getMinutes());
 
-  return ""+ year + "-" +
+  return ""+ year + "-" + month + "-" + day + " " + hour + ":" + minute;
 
 }
+
+var addLeftCero = function (number) {
+
+  var intNumber = parseInt(number);
+
+  if(intNumber <10)
+    return "0" + intNumber;
+  else
+    return number;
+}
+
